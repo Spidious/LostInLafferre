f498b44897a6e4fedd1e4958b0f007093b9a6bd6
Anders4143 <anders4143@gmail.com>
2025-02-11 14:08:13 -0600

    functions moved into library

---
+++ main_func/src/main.rs
@@ -0,0 +1,24 @@
+use petgraph::{Graph, Undirected};
+use petgraph::graph::NodeIndex;
+use std::f64;
+use std::collections::HashMap;
+use graph_library::Coords;
+use graph_library::create_graph_from_json;
+use petgraph::dot::{Dot, Config};
+
+fn main(){
+
+    let path = "nodes_edges.json";
+    // Read the file into a string
+    let mut deps = Graph::<Coords, f64, Undirected>::new_undirected();
+    let mut room_gid: HashMap<String, NodeIndex> = HashMap::new();
+    let _ = create_graph_from_json(&mut deps, &mut room_gid, &path);
+
+    println!("Graph Nodes and edges");
+    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
+    println!("Hash Maps keys to indices");
+    for (key, value) in &room_gid {
+        println!("{} => {:?}", key, value);
+    }
+
+}
