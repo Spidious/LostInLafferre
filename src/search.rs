7f5dd60157924d8bf87b9b900ae93843dcd4cb91
Liam <lgreim33@gmail.com>
2025-02-18 17:06:36 -0600

    Removing old placeholder objects

--- src/search.rs
+++ src/search.rs
@@ -5,26 +5,6 @@ use petgraph::graph::Node;
 
 
 
-// Borrowed from Anders for consistency in model
-struct Coords(f64, f64, f64);
-
-impl Coords{
-    //3D euclidean distance function
-    fn euc_dist(&self,other: &Coords) -> f64{
-        f64::sqrt((self.0 - other.0)*(self.0 - other.0) +  
-                (self.1 - other.1)*(self.1 - other.1) + 
-                (self.2 - other.2)*(self.2 - other.2))
-    }
-}
-
-
-//place holder
-struct Node{
-    id: usize,
-    coods: Coords,
-}
-
-
 // State will be pushed to the open list
 struct State{
     node : Node,
