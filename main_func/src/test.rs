67baa19d7bd1728c4a7ec20e1dec6ca9bdce40b5
spidious <lukedeffenbaugh@gmail.com>
2025-04-28 19:20:58 -0500

    Create test file
    Signed-off-by: spidious <lukedeffenbaugh@gmail.com>

---
+++ main_func/src/test.rs
@@ -0,0 +1,65 @@
+// Test Basement to Third Floor
+#[cfg(test)]
+fn BtoTF() {
+    use super::*;
+}
+
+// First Floor to Second Floor Study Rooms
+#[cfg(test)]
+fn FFtoSF() {
+    use super::*;
+}
+
+// First Floor to First Floor
+#[cfg(test)]
+fn FFtoFF() {
+    use super::*;
+}
+
+// API Response
+#[cfg(test)]
+fn API_Resp() {
+    use super::*;
+}
+
+// Third Floor to Basement
+#[cfg(test)]
+fn TFtoB() {
+    use super::*;
+}
+
+// Optimal Stair/Elevator Selection
+#[cfg(test)]
+fn Stair_Elev() {
+    use super::*;
+}
+
+// Bad input from frontend
+#[cfg(test)]
+fn BadInput() {
+    use super::*;
+}
+
+// Multiple Request Handling
+#[cfg(test)]
+fn MultReq() {
+    use super::*;
+}
+
+// Create Graph from Json
+#[cfg(test)]
+fn GraphCreate() {
+    use super::*;
+}
+
+// 25+ rooms
+#[cfg(test)]
+fn CrashTest() {
+    use super::*;
+}
+
+// Same Room as Source and Destination
+#[cfg(test)]
+fn SameRoom() {
+    use super::*;
+}
